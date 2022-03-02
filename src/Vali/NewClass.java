8636edcfb94634d039ead1431c02fd0925bd2f84
Vala90 <junikuvalmira@gmail.com>
2022-03-02 09:55:19 +0100

    Rechnung hinzugefügt

--- src/Vali/NewClass.java
+++ src/Vali/NewClass.java
@@ -4,5 +4,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Vali");
+        System.out.println(12*24);
     }
 }
